d2a63ed9249bbcd097db0d95cba81142e6cefa6d
kodtanactc <49423222+kodtanactc@users.noreply.github.com>
2021-08-19 01:20:35 +0900

    asdas

--- unit_test.py
+++ unit_test.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 import pytest
-from get-data import get_redis_data
 def test_init():
     assert get_redis_data.RedisHost == "redis-13849.c9.us-east-1-4.ec2.cloud.redislabs.com"
